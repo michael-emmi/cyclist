db54e2073364937dd304813b9c7ab46f02560806
Michael Emmi <michael.emmi@gmail.com>
2015-11-13 14:30:54 +0100

    Several improvements.

--- bin/cyclist.rb
+++ bin/cyclist.rb
@@ -23,24 +23,29 @@ module SimpleInterpreter
 end
 
 module EventActionInterpreter
-  E = {
-    # Start and complete actions are implicit.
-    access: /\AEvent:\s+(\d+|null)\s+[A-Z]{3}\s+(\d+)\s+.*\Z/,
-  }
-  
   def self.readline(line)
-    E.each do |act,expr|
-      line.match(expr) do |m|
-        return act, *m.to_a.drop(1).map(&:to_i)
-      end
+    line.match(%r{
+      \A
+      Event:\s+(?'event'\S+)
+      \s+
+      (?'action'\S+)\s+(?'object'\S+)\s+(?'type'\S+)\s+(?'field'\S+)
+      \s+
+      Thread:\s+(?'thread'.*)
+      \Z
+    }x) do |m|
+      return :access, m[:event], "#{m[:object]}.#{m[:field]}", m[:action]
     end
     puts "INVALID LINE: #{line}"
     return nil
   end
+  def self.conflict?(action1, action2)
+    action1 == 'PUT' || action2 == 'PUT'
+  end
 end
 
 class ConflictTracker
-  def initialize
+  def initialize(conflict_fn)
+    @conflict_fn = conflict_fn
     @objs = {}
     @accs = {}
     @future = {}
@@ -50,36 +55,38 @@ class ConflictTracker
 
   def agents;           @accs.keys end
   def live?(agent)      !@accs[agent].nil? end
-  def accesses(agent)   @accs[agent] + @future[agent] end
   def cycle?(agent)     @preds[agent].include?(agent) end
+
+  def accesses(agent)
+    @accs[agent].merge(@future[agent]){|_,ms1,ms2| ms1.merge(ms2)}
+  end
 
   def to_s
     return "no live agents" unless agents.count > 0
     agents.map do |a|
-      "agent #{a}: accesses {#{@accs[a].map(&:to_s) * ", "}} " +
-      "future {#{@future[a].map(&:to_s) * ", "}} " +
+      "agent #{a}: accesses {#{@accs[a].map{|o,ms| "#{o}(#{ms.map(&:to_s) * ","})"} * ", "}} " +
+      "future {#{@future[a].map{|o,k| "#{o}(#{ms.map(&:to_s) * ","})"} * ", "}} " +
       "before {#{@succs[a].map(&:to_s) * ", "}}"
     end * "\n"
   end
 
   def start(agent)
-    @accs[agent] = Set.new
-    @future[agent] = Set.new
+    @accs[agent] = {}
+    @future[agent] = {}
     @preds[agent] = Set.new
     @succs[agent] = Set.new
   end
 
   def complete(agent)
     return unless live?(agent)
-
-    accesses(agent).each do |o|
+    accesses(agent).each do |o,ms|
       @objs[o].merge(@preds[agent])
       @objs[o].delete(agent)
     end
     @preds[agent].each do |a|
       @succs[a].merge(@succs[agent])
       @succs[a].delete(agent)
-      @future[a].merge(accesses(agent))
+      @future[a].merge(accesses(agent)){|_,ms1,ms2| ms1.merge(ms2)}
     end
     @succs[agent].each do |a|
       @preds[a].merge(@preds[agent])
@@ -91,16 +98,20 @@ class ConflictTracker
     @succs.delete(agent)
   end
 
-  def access(agent, object)
+  def access(agent, object, method)
     start(agent) unless live?(agent)
     @objs[object] ||= Set.new
     @objs[object].each do |a|
-      next if agent == a && !@future[a].include?(object)
-      @preds[agent] << a
-      @succs[a] << agent
+      if @future[a][object] && @future[a][object].any?{|m| @conflict_fn.call(m,method)} ||
+        agent != a && @accs[a][object] && @accs[a][object].any?{|m| @conflict_fn.call(m,method)}
+      then
+        @preds[agent] << a
+        @succs[a] << agent        
+      end
     end
     @objs[object] << agent
-    @accs[agent] << object
+    @accs[agent][object] ||= Set.new
+    @accs[agent][object] << method
   end
 end
 
@@ -136,13 +147,15 @@ begin
     end
   end.parse!
   
+  puts "Serious Cyclist version 0.3"
+
   execution_log = ARGV.first
   unless execution_log && File.exists?(execution_log)
     puts "INVALID/MISSING EXECUTION LOG FILE #{execution_log}"
     exit
   end
 
-  tracker = ConflictTracker.new
+  tracker = ConflictTracker.new(EventActionInterpreter.method(:conflict?))
   interpreter = EventActionInterpreter
   agents = Set.new
   objects = Set.new
@@ -170,10 +183,13 @@ begin
     steps += 1
   end
 
+  puts "-" * 60
+  puts "trace:  #{execution_log}"
   puts "agents: #{agents.count}"
   puts "objets: #{objects.count}"
   puts "steps:  #{steps}"
   puts "cycle:  #{cycle}"
   puts "time:   #{(Time.now - start_time).round(3)}s"
+  puts "-" * 60
 
 end
